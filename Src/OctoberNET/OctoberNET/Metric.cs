346977ae36e41f0cdec8ab2491592f0a03809008
Mahasak Pijittum <mahasak.pijittum@agoda.com>
2017-01-14 17:03:09 +0700

    Add mock project and test

---
+++ Src/OctoberNET/OctoberNET/Metric.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OctoberNet
+{
+    public static class Metric
+    {
+        public static bool CreateMeasurement(string name)
+        {
+            return true;
+        }
+    }
+}
---
+++ Src/OctoberNet.Test/CoreTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using OctoberNet;
+using NUnit.Framework;
+
+namespace OctoberNet.Test
+{
+    [TestFixture]
+    public class CoreTest
+    {
+        [Test]
+        public void CreateMeasurementShouldReturnTrueOnSuccess()
+        {
+            Assert.IsTrue(OctoberNet.Metric.CreateMeasurement("test"));
+        }
+    }
+}
